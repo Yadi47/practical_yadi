1e653f810ddac250eda2335475e119d24f667fe8
Yadi47 <krissetiyadi@gmail.com>
2020-12-22 16:51:10 +0700

    test cok2

--- resources/views/layouts/sidebar_customer.blade.php
+++ resources/views/layouts/sidebar_customer.blade.php
@@ -12,7 +12,7 @@
                 <div class="pull-left info">
                     <p>{{ \Auth::user()->name  }}</p>
                     <!-- Status -->
-                    <a href="#"><i class="fa fa-circle text-success"></i> Online</a>
+                    <a href="#"><i class="fa fa-circle text-success"></i> Hai cok</a>
                 </div>
             </div>
 
@@ -69,8 +69,8 @@
                     <i class="fa fa-th"></i>
                     <span>Category Test</span>
                     <span class="pull-right-container">
-              <i class="fa fa-angle-left pull-right"></i>
-            </span>
+                <i class="fa fa-angle-left pull-right"></i>
+                </span>
                 </a>
                 <ul class="treeview-menu">
                     @foreach(\App\Category::all() as $category)
